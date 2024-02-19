2afcdbe9c94561d557d29fc90040c9c53e0f4df1
Samar <osmansamar367@gmail.com>
2024-02-19 19:43:48 +0100

    Added client and server Sides

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+// import Books from "./components/Books";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Books.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Books = () => {
+  return <div className="text-green-500">Books</div>;
+};
+
+export default Books;
